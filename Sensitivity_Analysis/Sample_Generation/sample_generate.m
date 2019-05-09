e82c44007b0d37bb8f23d9f78b16cd5ea612dc2e
grace-y <50380991+grace-y@users.noreply.github.com>
2019-05-09 16:38:40 -0400

    Update sample_generate.m

--- Sensitivity_Analysis/Sample_Generation/sample_generate.m
+++ Sensitivity_Analysis/Sample_Generation/sample_generate.m
@@ -1,25 +1,23 @@
 close all;
 clear all;
 
-par = xlsread('parameters.xlsx','sheet1');
+par = xlsread('parameters.xlsx','sheet1'); # location of parameter specifications (number, mean, SD, range, and type*)
 size=5400;
 for i=1:213
     stats(i,1) = i;
     a = par(i,4);
     b = par(i,5);
-	% For lognormal distibution (Type 1)
+    
+    % For lognormal distibution (*Type 1)
     if par(i,6)==1
         r = (b-a).*rand(size,1) + a; 
-
-         %r_range = [min(r) max(r)]
-         %r = lognrnd(par(i,2),par(i,3),2000,1);
         j(:,i)=r;
         histfit(r);
         skewness(r);
         stats(i,2) = mean(log(r));
         stats(i,3) = std(log(r));
         
-    % For discrete lognormal distibution (Type 2)
+    % For discrete lognormal distibution (*Type 2)
 	elseif par(i,6)==2        
         x = (b-a).*rand(size,1) + a; 
         y=floor(x);
@@ -36,19 +34,14 @@ for i=1:213
         stats(i,2) = mean(log(r));
         stats(i,3) = std(log(r));     
                
-    % For normal distibution (Type 3, half life)
+    % For normal distibution (*Type 3, half life)
 	elseif par(i,6)==3
         r = (b-a).*rand(size,1) + a; 
         j(:,i)=r;
-%         for m=1:100
-%             j(m,i)=power(0.5,30/(r(m,1)));
-%         end    
         histfit(r);
         skewness(r);
         stats(i,2) = mean(r);
         stats(i,3) = std(r);
-        
-       
         
     end 
     stats(i,4) = min(r);
